1954e5f3c4d7c95c2bb4879db620d53b10db7d85
maxens-git <61346038+maxens-git@users.noreply.github.com>
2024-04-17 19:15:40 +0200

    Update infos.php

--- email_pixel/infos.php
+++ email_pixel/infos.php
@@ -1,58 +1,5 @@
 <?php
 
-// URL of the target server
-$url = "https://maxens-ntfy.duckdns.org/Raspberry";
-
-// Data to send in the POST request (replace with your actual data)
-$data = array(
-  'content' => 'John Doe'
-);
-
-// Headers to include in the request (replace with your specific headers)
-$headers = array(
-  'Content-Type: application/json', // Example header for JSON data
-  'Authorization: Bearer REDACTED_TOKEN', // Example authorization header
-);
-
-// Initialize a new cURL resource
-$ch = curl_init($url);
-
-// Set request method to POST
-curl_setopt($ch, CURLOPT_POST, true);
-
-// Set POST data
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data)); // Assuming JSON data
-
-// Set headers
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-// Return the transfer as a string instead of outputting it directly
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// Verify SSL certificate (optional, adjust based on your server's setup)
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-// Execute the request
-$response = curl_exec($ch);
-
-// Check for errors
-if (curl_errno($ch)) {
-  echo 'Error: ' . curl_error($ch);
-} else {
-  // Process the response
-  echo $response;
-}
-
-// Close the cURL resource
-curl_close($ch);
-
-?>
-ubuntu@instance-20230801-1801:/docker/email_pixel$ sudo nano tracker.php 
-ubuntu@instance-20230801-1801:/docker/email_pixel$ sudo nano tracker.php 
-ubuntu@instance-20230801-1801:/docker/email_pixel$ sudo cat tracker.php 
-<?php
-
 //THIS RETURNS THE IMAGE
 header('Content-Type: image/gif');
 readfile('tracking.gif');
